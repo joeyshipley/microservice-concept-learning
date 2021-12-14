9fb66de52bdd087cecc080fbe725dd1202dc94bf
Joey Shipley <jshipley@8thlight.com>
2021-12-14 16:04:49 -0600

    setting up message event reading for history

--- stream-history/src/index.js
+++ stream-history/src/index.js
@@ -1,55 +1,96 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongodb = require('mongodb');
+const amqp = require('amqplib');
 const dayjs = require('dayjs');
 
 const PORT = process.env.PORT;
 const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
+const RABBIT = process.env.RABBIT;
 
-function setupHandlers(app) {
+const app = express();
+
+function main() {
   return mongodb.MongoClient
     .connect(DBHOST)
     .then((client) => {
-      const db = client.db(DBNAME);
-      const videosViewedCollection = db.collection('videos-viewed');
+      return connectRabbit()
+        .then((messageChannel) => {
+          const db = client.db(DBNAME);
+          const videosViewedCollection = db.collection('videos-viewed');
 
-      app.get('/', (req, res) => { res.send(`TEST Stream-History IS GO! ${ dayjs().format('YYYY-MM-DD HH:mm:ss')  }`); });
+          function consumeVideoViewedMessage(msg) {
+            const data = JSON.parse(msg.content.toString());
+            console.log(` --- HISTORY EVENT --- VIEWED <------------------------------------------------ `);
+            console.log(data);
+            videosViewedCollection.insertOne({ videoId: data.videoId, viewedOn: data.viewedOn })
+              .then(() => {
+                console.log(`Stream-History :: Video Watched ID=${ data.videoId }`);
+              })
+              .catch((err) => {
+                console.error(`Stream-History :: unable to add video watched for ID=${ videoId }`);
+                console.error(err);
+              });
+          }
 
-      app.post('/viewed', (req, res) => {
-        const videoId = req.body.videoId;
-        const viewedOn = req.body.viewedOn;
-        console.log(` ---------- VIDEO VIEWED! [ ${ viewedOn } ] ---------- `);
-        videosViewedCollection.insertOne({ videoId: videoId, viewedOn: viewedOn })
-          .then(() => {
-            console.log(`Stream-History :: Video Watched ID=${ videoId }`);
-            res.sendStatus(200);
-          })
-          .catch((err) => {
-            console.error(`Stream-History :: unable to add video watched for ID=${ videoId }`);
-            console.error(err);
-            res.sendStatus(500);
+          app.use(bodyParser.json());
+
+          app.get('/', (req, res) => { res.send(`TEST Stream-History IS GO! ${ dayjs().format('YYYY-MM-DD HH:mm:ss')  }`); });
+
+          // app.post('/viewed', (req, res) => {
+          //   const videoId = req.body.videoId;
+          //   const viewedOn = req.body.viewedOn;
+          //   console.log(` ---------- VIDEO VIEWED! [ ${ viewedOn } ] ---------- `);
+          //   videosViewedCollection.insertOne({ videoId: videoId, viewedOn: viewedOn })
+          //     .then(() => {
+          //       console.log(`Stream-History :: Video Watched ID=${ videoId }`);
+          //       res.sendStatus(200);
+          //     })
+          //     .catch((err) => {
+          //       console.error(`Stream-History :: unable to add video watched for ID=${ videoId }`);
+          //       console.error(err);
+          //       res.sendStatus(500);
+          //     });
+          // });
+
+          app.listen(PORT, () => {
+            console.log(`Stream-History :: Service ONLINE (PORT ${ PORT })`);
           });
-      });
+
+          return messageChannel.assertQueue('video-viewed', {})
+            .then(() => {
+              return messageChannel.consume('video-viewed', consumeVideoViewedMessage);
+            });
+        })
+        .catch((err) => {
+          console.error("RabbitMQ connection failure.");
+          console.error(err);
+          throw err;
+        });
+    })
+    .catch((err) => {
+      console.error("MongoDB connection failure.");
+      console.error(err);
+      throw err;
     });
 }
 
-function startHttpServer() {
-  return new Promise((resolve) => {
-    const app = express();
-
-    app.use(bodyParser.json());
-    setupHandlers(app);
-
-    app.listen(PORT, () => { resolve(); });
-  });
+function connectRabbit() {
+  return amqp
+    .connect(RABBIT)
+    .then((messagingConnection) => messagingConnection.createChannel());
 }
 
-startHttpServer()
-  .then(() => {
-    console.log(`Stream-History :: Service ONLINE (PORT ${ PORT })`);
-  })
-  .catch((err) => {
-    console.error(`Stream-History :: Service FAILURE (PORT ${ PORT })`);
-    console.error(err);
-  });
+function startService() {
+  return main()
+    .catch((err) => {
+      console.error(`Stream-History :: Service FAILURE (PORT ${ PORT })`);
+      console.error(err);
+      const restartIn = 5000;
+      console.log(`Stream-History :: Queuing Restart (${ restartIn })`);
+      setTimeout(startService, restartIn, 'restart-service');
+    });
+}
+
+startService();
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const mongodb = require('mongodb');
+const amqp = require('amqplib');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 dayjs.extend(utc);
@@ -10,6 +11,9 @@ const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
+const RABBIT = process.env.RABBIT;
+
+console.log(` --> ${ RABBIT } <-------------------------------------------------`);
 
 const app = express();
 
@@ -17,80 +21,92 @@ function main() {
   return mongodb.MongoClient
     .connect(DBHOST)
     .then((client) => {
-      const db = client.db(DBNAME);
-      const videosCollection = db.collection('videos');
+      return connectRabbit()
+        .then((messageChannel) => {
+          const db = client.db(DBNAME);
+          const videosCollection = db.collection('videos');
 
-      app.get('/', (req, res) => { res.send(`Video-Streaming IS GO! ${ dayjs().format('YYYY-MM-DD HH:mm:ss') }`); });
+          app.get('/', (req, res) => { res.send(`Video-Streaming IS GO! ${ dayjs().format('YYYY-MM-DD HH:mm:ss') }`); });
 
-      app.get('/video', (req, res) => {
-        console.log(' ------------------ STREAM-IT > View View REQUEST! ------------------ ');
-        const videoId = new mongodb.ObjectID(req.query.id);
-        videosCollection
-          .findOne({ _id: videoId })
-          .then((videoRecord) => {
-            if(!videoRecord) {
-              console.log(`Video (${ videoId }) not found in DB.`);
-              res.sendStatus(404);
-              return;
-            }
+          app.get('/video', (req, res) => {
+            console.log(' ------------------ STREAM-IT > View View REQUEST! ------------------ ');
+            const videoId = new mongodb.ObjectID(req.query.id);
+            videosCollection
+              .findOne({ _id: videoId })
+              .then((videoRecord) => {
+                if(!videoRecord) {
+                  console.log(`Video (${ videoId }) not found in DB.`);
+                  res.sendStatus(404);
+                  return;
+                }
 
-            // NOTE: this style of passthrough request is causing Express
-            // to not send back the correct response type and ultimately
-            // ends up having the browser hit this endpoint twice.
-            const forwardRequest = http.request({
-                host: VIDEO_STORAGE_HOST,
-                port: VIDEO_STORAGE_PORT,
-                path: `/video?path=${ videoRecord.videoPath }`,
-                method: 'GET',
-                headers: req.headers
-            }, (forwardResponse) => {
-              res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-              forwardResponse.pipe(res);
-              produceEventForVideoViewed(videoId);
-            });
+                // NOTE: this style of passthrough request is causing Express
+                // to not send back the correct response type and ultimately
+                // ends up having the browser hit this endpoint twice.
+                const forwardRequest = http.request({
+                    host: VIDEO_STORAGE_HOST,
+                    port: VIDEO_STORAGE_PORT,
+                    path: `/video?path=${ videoRecord.videoPath }`,
+                    method: 'GET',
+                    headers: req.headers
+                }, (forwardResponse) => {
+                  res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+                  forwardResponse.pipe(res);
+                  produceEventForVideoViewed(messageChannel, videoId);
+                });
 
-            req.pipe(forwardRequest);
-          })
-          .catch((err) => {
-            console.error("Database query failure.");
-            console.error(err);
-            res.sendStatus(500);
+                req.pipe(forwardRequest);
+              })
+              .catch((err) => {
+                console.error("Database query failure.");
+                console.error(err);
+                res.sendStatus(500);
+              });
+
+            // produceEventForVideoViewed(videoId);
+            // res.send({ message: `Video Requested: ID ${ videoId }` });
           });
 
-        // produceEventForVideoViewed(videoId);
-        // res.send({ message: `Video Requested: ID ${ videoId }` });
-      });
-
-      app.listen(PORT, () => {
-        console.log(`Video-Streaming :: Service ONLINE (PORT ${ PORT })`);
-      });
+          app.listen(PORT, () => {
+            console.log(`Video-Streaming :: Service ONLINE (PORT ${ PORT })`);
+          });
+        })
+        .catch((err) => {
+          console.error("RabbitMQ connection failure.");
+          console.error(err);
+          throw err;
+        });
+    })
+    .catch((err) => {
+      console.error("MongoDB connection failure.");
+      console.error(err);
+      throw err;
     });
 }
 
-function produceEventForVideoViewed(videoId) {
-    const options = {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const viewedOn = dayjs.utc().format();
-    const requestBody = {
-      videoId,
-      viewedOn,
-    };
-    const req = http.request("http://stream-history/viewed", options);
-    req.on("close", () => {});
-    req.on("error", (error) => {
-      console.log('Error sending video viewed event to history.');
-      console.log(error);
-    });
-    req.write(JSON.stringify(requestBody));
-    req.end();
+function connectRabbit() {
+  return amqp
+    .connect(RABBIT)
+    .then((messagingConnection) => messagingConnection.createChannel());
 }
 
-main()
-  .catch((err) => {
-    console.error(`Video-Streaming :: Service FAILURE (PORT ${ PORT })`);
-    console.error(err);
-  });
+function produceEventForVideoViewed(messageChannel, videoId) {
+  const data = {
+    videoId,
+    viewedOn: dayjs.utc().format(),
+  };
+  messageChannel.publish('', "video-viewed", Buffer.from(JSON.stringify(data)));
+}
+
+function startService() {
+  return main()
+    .catch((err) => {
+      console.error(`Video-Streaming :: Service FAILURE (PORT ${ PORT })`);
+      console.error(err);
+      const restartIn = 5000;
+      console.log(`Video-Streaming :: Queuing Restart (${ restartIn })`);
+      setTimeout(startService, restartIn, 'restart-service');
+    });
+}
+
+startService();
